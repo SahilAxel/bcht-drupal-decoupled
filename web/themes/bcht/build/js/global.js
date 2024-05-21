f56602345e28b67492b16b8b1c8190cee932f44b
Nazrul Islam <nazrulislambhat@gmail.com>
2024-05-21 11:03:22 +0530

    moved theme inside custom folder

--- web/themes/bcht/build/js/global.js
+++
@@ -1,2 +0,0 @@
-!function(o){o.behaviors.global={attach:(o,l)=>{console.log("Hello test")}}}(Drupal,(jQuery,once));
-//# sourceMappingURL=global.js.map
--- web/themes/bcht/src/js/global.js
+++
@@ -1,7 +0,0 @@
-(function (Drupal, $, once) {
-  Drupal.behaviors.global = {
-    attach: (context, settings) => {
-      console.log("Hello test")
-    },
-  };
-}(Drupal, jQuery, once));
