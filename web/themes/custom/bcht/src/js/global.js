99a070b0900c460b1ac3595bf63d5b64e1e85647
Shyam Bhatt <shyam.bhatt@axelerant.com>
2024-06-03 19:11:15 +0530

    BCHT-67: Added CSS, JS and twig file for the Hero banner component and Generic page layout.

--- web/themes/custom/bcht/src/js/global.js
+++ web/themes/custom/bcht/src/js/global.js
@@ -1,7 +1,19 @@
-(function (Drupal, $) {
+(function (Drupal, $, once) {
   Drupal.behaviors.global = {
-    attach: function (context, settings) {
-      console.log('Hello World');
+    attach: function (context) {
+      // Back to top JS
+      var $backToTop = $('.back-to-top');
+      $backToTop.hide();
+      $(window).on('scroll', function () {
+        if ($(this).scrollTop() > 100) {
+          $backToTop.fadeIn();
+        } else {
+          $backToTop.fadeOut();
+        }
+      });
+      $(once('backToTop', $backToTop, context)).click(function () {
+        $('html, body').animate({ scrollTop: 0 }, 500);
+      });
     },
   };
-})(Drupal, jQuery);
+})(Drupal, jQuery, once);
