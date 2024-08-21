5f2d7c434bdbefda13b7a85b66342414e885c1b2
SahilAxel <sahil.gidwani@axelerant.com>
2024-08-21 14:15:32 +0530

    BCHT-88: Implemented PR review changes

--- web/modules/custom/bcht_helpers/src/Routing/RouteSubscriber.php
+++ web/modules/custom/bcht_helpers/src/Routing/RouteSubscriber.php
@@ -8,16 +8,17 @@ use Symfony\Component\Routing\RouteCollection;
 /**
  * Listens to the dynamic route events.
  */
-class RouteSubscriber extends RouteSubscriberBase
-{
+class RouteSubscriber extends RouteSubscriberBase {
 
   /**
    * {@inheritdoc}
    */
-  protected function alterRoutes(RouteCollection $collection)
-  {
-    if ($route = $collection->get('entity.asset_injector_css.collection')) {
+  protected function alterRoutes(RouteCollection $collection) {
+    // Removes access for CSS injector admin page for all users including admins.
+    $route = $collection->get('entity.asset_injector_css.collection');
+    if ($route) {
       $route->setRequirement('_access', 'FALSE');
     }
   }
+
 }
